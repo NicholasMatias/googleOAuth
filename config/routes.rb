ccb7602e7550de10e410cd69488b9fd7e660c946
NicholasMatias <n-r-matias@outlook.com>
2025-02-10 09:18:29 +0000

    google oauth is officially up and running

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
     get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
   end
-  resource :books
+  resources :books
 end
 
 
