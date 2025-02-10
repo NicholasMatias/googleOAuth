464b090fc26d3b6e7e643a8ef5659f2316434cac
NicholasMatias <n-r-matias@outlook.com>
2025-02-10 08:30:22 +0000

    added google oauth => need to add user authorization now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :books
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'books#index'
+  devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks', sessions: 'admins/sessions'}
+  devise_scope :admin do
+    get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
+    get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
+  end
+  resource :books
 end
+
+
+#http://127.0.0.1:3000/
